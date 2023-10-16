45ceb36792fb7b84628b6d8fe7d8fa6f2644bf7a
Skolens Sk <skolens@skola.madonasvsk.lv>
2023-10-16 09:34:53 +0300

    text

--- 6.py
+++ 6.py
@@ -8,4 +8,8 @@ git add . - add to somthing
 git commit -m "Init commit" - commit the stuff
 git remote add origin - add a place 
 git push -u origin master - does some pushing
+to add new stuff do
+git add .
+git commit -m "text"
+git push
 """
