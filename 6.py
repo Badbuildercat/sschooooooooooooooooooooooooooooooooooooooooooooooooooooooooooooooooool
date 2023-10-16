01ae36b50cfa9d629b266b3191daad6db893691b
Skolens Sk <skolens@skola.madonasvsk.lv>
2023-10-16 09:33:18 +0300

    please

--- 6.py
+++ 6.py
@@ -1,5 +1,11 @@
 #version control tools
 #repository where you shove yor stuff
 """
+git commands 
+
 git init - initialize 
+git add . - add to somthing
+git commit -m "Init commit" - commit the stuff
+git remote add origin - add a place 
+git push -u origin master - does some pushing
 """
